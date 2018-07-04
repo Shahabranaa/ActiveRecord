e051a977003e0fb01b609dab0ffb9349039ae106
Shahab Rana <shahabranaa@gmail.com>
2018-07-04 14:39:20 +0500

    Assoiciations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
+  has_many :reviews
 
   after_create do |product|
       puts 'You have initialized an object!'
     end
     around_save do |product|
       puts 'Object is being saved'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   get 'active_record/migration'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+  resources :reviews
+  end
+
 
   root 'active_record#migration'
 end
